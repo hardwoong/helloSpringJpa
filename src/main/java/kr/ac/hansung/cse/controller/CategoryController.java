83ba89ace452a01cf7261655a0b97b0659a9fa8c
hardwoong <dew04300@naver.com>
2026-04-16 21:33:35 +0900

    feat: Category 중복 검사(findByName)와 커스텀 예외 처리 구현

--- src/main/java/kr/ac/hansung/cse/controller/CategoryController.java
+++ src/main/java/kr/ac/hansung/cse/controller/CategoryController.java
@@ -1,17 +1,27 @@
 package kr.ac.hansung.cse.controller;
 
+import jakarta.validation.Valid;
+import kr.ac.hansung.cse.exception.DuplicateCategoryException;
 import kr.ac.hansung.cse.model.CategoryForm;
+import kr.ac.hansung.cse.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/categories")
 public class CategoryController {
 
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping
     public String list(Model model) {
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "categoryList";
     }
 
@@ -19,5 +29,22 @@ public class CategoryController {
     public String createForm(Model model) {
         model.addAttribute("categoryForm", new CategoryForm());
         return "categoryForm";
+    }
+
+    @PostMapping("/create")
+    public String create(@Valid @ModelAttribute CategoryForm categoryForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "categoryForm";
+        }
+
+        try {
+            categoryService.createCategory(categoryForm.getName());
+        } catch (DuplicateCategoryException e) {
+            bindingResult.rejectValue("name", "duplicate", e.getMessage());
+            return "categoryForm";
+        }
+
+        return "redirect:/categories";
     }
 }
---
+++ src/main/java/kr/ac/hansung/cse/service/CategoryService.java
@@ -0,0 +1,33 @@
+package kr.ac.hansung.cse.service;
+
+import kr.ac.hansung.cse.exception.DuplicateCategoryException;
+import kr.ac.hansung.cse.model.Category;
+import kr.ac.hansung.cse.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Transactional
+    public Category createCategory(String name) {
+        if (categoryRepository.findByName(name).isPresent()) {
+            throw new DuplicateCategoryException(name);
+        }
+
+        return categoryRepository.save(new Category(name));
+    }
+}
